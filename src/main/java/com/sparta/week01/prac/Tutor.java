4ab50c56873fdfd8e1c76865237bbdb6fafb8fa9
배소정 <thfakdma2015@naver.com>
2022-11-08 18:09:22 +0900

    1주차 10강 완료

---
+++ src/main/java/com/sparta/week01/prac/Tutor.java
@@ -0,0 +1,31 @@
+package com.sparta.week01.prac;
+
+public class Tutor {
+    private String name;
+    private String bio;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBio() {
+        return this.bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public Tutor() {
+    }
+
+    public Tutor(String name, String bio) {
+        this.name = name;
+        this.bio = bio;
+    }
+}
+
