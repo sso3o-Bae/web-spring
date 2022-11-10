fd4e24dd135b475ab56e206309bb2d8d0d4356c8
배소정 <thfakdma2015@naver.com>
2022-11-10 18:42:21 +0900

    3주차 5강 완료

---
+++ src/main/java/com/sparta/webSpring/controller/MemoController.java
@@ -0,0 +1,40 @@
+package com.sparta.webSpring.controller;
+
+import com.sparta.webSpring.domain.Memo;
+import com.sparta.webSpring.domain.MemoRepository;
+import com.sparta.webSpring.domain.MemoRequestDto;
+import com.sparta.webSpring.service.MemoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class MemoController {
+
+    private final MemoRepository memoRepository;
+    private final MemoService memoService;
+
+    @PostMapping("/api/memos")
+    public Memo createMemo(@RequestBody MemoRequestDto requestDto) {
+        Memo memo = new Memo(requestDto);
+        return memoRepository.save(memo);
+    }
+
+    @GetMapping("/api/memos")
+    public List<Memo> getMemos() {
+        return memoRepository.findAllByOrderByModifiedAtDesc();
+    }
+
+    @DeleteMapping("/api/memos/{id}")
+    public Long deleteMemo(@PathVariable Long id) {
+        memoRepository.deleteById(id);
+        return id;
+    }
+
+    @PutMapping("/api/memos/{id}")
+    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto){
+        return memoService.update(id, requestDto);
+    }
+}
