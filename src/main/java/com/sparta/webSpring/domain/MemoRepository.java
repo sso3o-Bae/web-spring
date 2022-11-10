168637b9a566d645bcf7ac22d666c411fb73c32f
배소정 <thfakdma2015@naver.com>
2022-11-10 20:51:41 +0900

    3주차 완강

--- src/main/java/com/sparta/webSpring/domain/MemoRepository.java
+++ src/main/java/com/sparta/webSpring/domain/MemoRepository.java
@@ -2,8 +2,9 @@ package com.sparta.webSpring.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface MemoRepository extends JpaRepository<Memo, Long> {
-    List<Memo> findAllByOrderByModifiedAtDesc(); //다 찾아주고, 정렬을 해줘 수정된 날짜를 기준으로, 최신순으로
+    List<Memo> findAllByModifiedAtBetweenOrderByModifiedAtDesc(LocalDateTime start, LocalDateTime end); //다 찾아주고, 정렬을 해줘 수정된 날짜를 기준으로, 최신순으로 //24시간 이내만
 }
