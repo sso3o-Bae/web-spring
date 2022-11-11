9c6e0458866d2d18350dfc35e67db273740c032e
배소정 <thfakdma2015@naver.com>
2022-11-11 21:21:00 +0900

    4주차 4강 완료

---
+++ src/main/java/com/sparta/webSpring/utils/NaverShopSearch.java
@@ -0,0 +1,29 @@
+package com.sparta.webSpring.utils;
+
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
+
+public class NaverShopSearch {
+    public String search() {
+        RestTemplate rest = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-Naver-Client-Id", "WqO763dhu8bPUdm4BgX8");
+        headers.add("X-Naver-Client-Secret", "09tDs7tYUP");
+        String body = "";
+
+        HttpEntity<String> requestEntity = new HttpEntity<String>(body, headers);
+        ResponseEntity<String> responseEntity = rest.exchange("https://openapi.naver.com/v1/search/shop.json?query=아디다스", HttpMethod.GET, requestEntity, String.class);
+        HttpStatus httpStatus = responseEntity.getStatusCode();
+        int status = httpStatus.value();
+        String response = responseEntity.getBody();
+        System.out.println("Response status: " + status);
+        System.out.println(response);
+
+        return response;
+    }
+
+    public static void main(String[] args) {
+        NaverShopSearch naverShopSearch = new NaverShopSearch();
+        naverShopSearch.search();
+    }
+}
