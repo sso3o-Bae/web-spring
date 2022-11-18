57f0b6baa8ba10fc80850b1055d395740e1a1884
배소정 <thfakdma2015@naver.com>
2022-11-18 23:31:46 +0900

    4주차 7강 완료

--- src/main/java/com/sparta/webSpring/prac/Course.java
+++ src/main/java/com/sparta/webSpring/prac/Course.java
@@ -1,7 +1,7 @@
 package com.sparta.webSpring.prac;
 
 public class Course {
-    // title, tutor, days 가 Course 라는 맥락 아래에서 의도가 분명히 드러나죠!
+    // title, tutor, days 가 Course 라는 맥락 아래에서 의도가 분명히 드러나죠! 1주차 강의.
     public String title;
     public String tutor;
     public int days;
