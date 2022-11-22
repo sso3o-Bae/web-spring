da0bddc5fc926fdf18d891fcce3df51e44f7872c
배소정 <thfakdma2015@naver.com>
2022-11-22 16:09:05 +0900

    4주차 11강 완료

--- src/main/resources/static/basic.js
+++ src/main/resources/static/basic.js
@@ -47,10 +47,29 @@ function execSearch() {
      * 검색결과 목록: #search-result-box
      * 검색결과 HTML 만드는 함수: addHTML
      */
-    // 1. 검색창의 입력값을 가져온다.
+        // 1. 검색창의 입력값을 가져온다.
+    let query = $('#query').val();
+
     // 2. 검색창 입력값을 검사하고, 입력하지 않았을 경우 focus.
+    if (query == '') {
+        alert('검색어를 입력해주세요');
+        $('#query').focus();
+        return;
+    }
     // 3. GET /api/search?query=${query} 요청
-    // 4. for 문마다 itemDto를 꺼내서 HTML 만들고 검색결과 목록에 붙이기!
+    $.ajax({
+        type: 'GET',
+        url: `/api/search?query=${query}`,
+        success: function (response) {
+            $('#search-result-box').empty();
+            // 4. for 문마다 itemDto를 꺼내서 HTML 만들고 검색결과 목록에 붙이기!
+            for (let i = 0; i < response.length; i++) {
+                let itemDto = response[i];
+                let tempHtml = addHTML(itemDto);
+                $('#search-result-box').append(tempHtml);
+            }
+        }
+    })
 
 }
 
@@ -60,7 +79,21 @@ function addHTML(itemDto) {
      * image, title, lprice, addProduct 활용하기
      * 참고) onclick='addProduct(${JSON.stringify(itemDto)})'
      */
-    return ``
+    return `<div class="search-itemDto">
+        <div class="search-itemDto-left">
+            <img src="${itemDto.image}" alt="">
+        </div>
+        <div class="search-itemDto-center">
+            <div>${itemDto.title}</div>
+            <div class="price">
+                ${numberWithCommas(itemDto.lprice)}
+                <span class="unit">원</span>
+            </div>
+        </div>
+        <div class="search-itemDto-right">
+            <img src="images/icon-save.png" alt="" onclick='addProduct(${JSON.stringify(itemDto)})'>
+        </div>
+    </div>`
 }
 
 function addProduct(itemDto) {
@@ -71,7 +104,17 @@ function addProduct(itemDto) {
      * 2. data: JSON.stringify(itemDto),
      */
     // 1. POST /api/products 에 관심 상품 생성 요청
-    // 2. 응답 함수에서 modal을 뜨게 하고, targetId 를 reponse.id 로 설정 (숙제로 myprice 설정하기 위함)
+    $.ajax({
+        type: "POST",
+        url: '/api/products',
+        contentType: "application/json",
+        data: JSON.stringify(itemDto),
+        success: function (response) {
+            // 2. 응답 함수에서 modal을 뜨게 하고, targetId 를 reponse.id 로 설정 (숙제로 myprice 설정하기 위함)
+            $('#container').addClass('active');
+            targetId = response.id;
+        }
+    })
 }
 
 function showProduct() {
@@ -80,14 +123,44 @@ function showProduct() {
      * 검색결과 목록: #search-result-box
      * 관심상품 HTML 만드는 함수: addProductItem
      */
+
     // 1. GET /api/products 요청
-    // 2. 관심상품 목록, 검색결과 목록 비우기
-    // 3. for 문마다 관심 상품 HTML 만들어서 관심상품 목록에 붙이기!
+    $.ajax({
+        type: 'GET',
+        url: '/api/products',
+        success: function (response) {
+            // 2. 관심상품 목록, 검색결과 목록 비우기
+            $('#product-container').empty();
+            $('#search-result-box').empty();
+            // 3. for 문마다 관심 상품 HTML 만들어서 관심상품 목록에 붙이기!
+            for (let i = 0; i < response.length; i++) {
+                let product = response[i];
+                let tempHtml = addProductItem(product);
+                $('#product-container').append(tempHtml);
+            }
+        }
+    })
 }
 
 function addProductItem(product) {
     // link, image, title, lprice, myprice 변수 활용하기
-    return ``;
+    return `<div class="product-card" onclick="window.location.href='${product.link}'">
+                <div class="card-header">
+                    <img src="${product.image}"
+                         alt="">
+                </div>
+                <div class="card-body">
+                    <div class="title">
+                        ${product.title}
+                    </div>
+                    <div class="lprice">
+                        <span>${numberWithCommas(product.lprice)}</span>원
+                    </div>
+                    <div class="isgood ${product.lprice > product.myprice ? 'none' : ''}">
+                        최저가
+                    </div>
+                </div>
+            </div>`;
 }
 
 function setMyprice() {
