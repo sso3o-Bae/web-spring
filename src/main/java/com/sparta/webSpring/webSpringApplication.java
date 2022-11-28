2287197f9000b57c613e34d06a140eaeba99f63d
배소정 <thfakdma2015@naver.com>
2022-11-28 22:49:00 +0900

    심화 1주차 시작

--- src/main/java/com/sparta/webSpring/webSpringApplication.java
+++ src/main/java/com/sparta/webSpring/webSpringApplication.java
@@ -2,12 +2,14 @@ package com.sparta.webSpring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling // 스프링 부트에서 스케줄러가 작동하게 합니다.
 @EnableJpaAuditing // 시간 자동 변경이 가능하도록 합니다.
 @SpringBootApplication // 스프링 부트임을 선언합니다.
+@ServletComponentScan // @WebServlet 어노테이션이 동작하게 함
 public class webSpringApplication {
 
     public static void main(String[] args) {
