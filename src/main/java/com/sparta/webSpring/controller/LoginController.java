5582426b9b33926476d7c517c81013ba2d8c006a
배소정 <thfakdma2015@naver.com>
2022-12-08 23:27:47 +0900

    MVC

--- src/main/java/com/sparta/webSpring/controller/LoginController.java
+++ src/main/java/com/sparta/webSpring/controller/LoginController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+//MVC
 @Controller
 public class LoginController {
 
